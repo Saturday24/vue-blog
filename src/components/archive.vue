edfe6cb0e6484e4e042613c3cd40aa3b24fd380c
csf_mac <18340836866@163.com>
2018-12-11 17:30:15 +0800

    modified header content

--- src/components/archive.vue
+++ src/components/archive.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="archive">
     <div class="bgBanner">
+      <p class="bannerTitle">{{bannerTitle}}</p>
+      <p class="bannerDesc">{{bannerDesc}}</p>
     </div>
   </div>
 </template>
@@ -10,6 +12,8 @@ export default {
   name: 'archive',
   data () {
     return {
+      bannerTitle: 'Leo Chen',
+      bannerDesc: 'Not Perfect, So Need To Learn'
     }
   }
 }
@@ -25,5 +29,26 @@ export default {
       background-image: url(../common/image/post-bg.jpg);
       background-repeat: no-repeat;
       background-size: 100% 100%;
+      position: relative;
+  }
+  .bannerTitle {
+    color: #fff;
+    font-size: 40px;
+    font-weight: bold;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -80px;
+    margin-top: -30px;
+  }
+  .bannerDesc {
+    color: #7e7e7e;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -110px;
+    margin-top: 20px;
+    font-size: 18px;
   }
 </style>
+
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="home">
     <div class="bgBanner">
+      <p class="bannerTitle">{{bannerTitle}}</p>
+      <p class="bannerDesc">{{bannerDesc}}</p>
     </div>
   </div>
 </template>
@@ -10,6 +12,8 @@ export default {
   name: 'home',
   data () {
     return {
+      bannerTitle: 'Leo Chen',
+      bannerDesc: 'Not Perfect, So Need To Learn'
     }
   }
 }
@@ -25,5 +29,25 @@ export default {
       background-image: url(../common/image/home-bg.jpg);
       background-repeat: no-repeat;
       background-size: 100% 100%;
+      position: relative;
+  }
+  .bannerTitle {
+    color: #fff;
+    font-size: 40px;
+    font-weight: bold;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -80px;
+    margin-top: -30px;
+  }
+  .bannerDesc {
+    color: #7e7e7e;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-left: -110px;
+    margin-top: 20px;
+    font-size: 18px;
   }
 </style>
